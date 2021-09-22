1b6508a1d732a2d1e1f2503f1960c5446750d75c
Magicxavi <rolfpeterkesche@gmail.com>
2021-09-22 03:53:56 +0300

    sdm845-common: add custom apns-conf
    E/TelephonyProvider(2846): FileNotFoundException for /system/etc/apns-conf.xml:java.io.FileNotFoundException: /system/etc/apns-conf.xml: open failed: ENOENT (No such file or directory)

--- sdm845.mk
+++ sdm845.mk
@@ -16,6 +16,10 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
