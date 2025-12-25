5280376be07aa7e7125955e23c038dff21a0ef94
Ibra Al Tabian <ibratabian@gmail.com>
2025-12-25 13:23:35 +0700

    sdm845-common: init halcyon

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,7 +109,7 @@ BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 5163188224 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB overhead
 
-include vendor/lineage/config/BoardConfigReservedSize.mk
+include vendor/halcyon/config/BoardConfigReservedSize.mk
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
